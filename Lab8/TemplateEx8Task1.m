fa71398e8a6b67ca0ee38b6596545a48d60a823d
cwr98 <corbanrosenauer@gmail.com>
2025-03-03 20:39:04 +0100

    updating and competing labs

--- Lab8/TemplateEx8Task1.m
+++ Lab8/TemplateEx8Task1.m
@@ -22,104 +22,105 @@ disp('Task 1 - Non-linear adjustment problem!')
 %   Observations and initial values for the unknowns
 %--------------------------------------------------------------------------
 %Vector of observations
-%L = 
+L = [-4; 8; 7.7;];
 
 %Number of observations
-%no_n = 
+no_n = length(L);
 
 %Initial values for the unknowns
-%x = 
-%y = 
+x = 2;
+y = 2;
 
 %Vector of initial values for the unknowns
-%X_0 = 
+X_0 = [x y]';
 
 %Number of unknowns
-%no_u = 
+no_u = length(X_0);
 
 %Redundancy
-%r =  
+r =  no_n - no_u;
 
 %--------------------------------------------------------------------------
 %  Stochastic model
 %--------------------------------------------------------------------------
 %VC Matrix of the observations
-%S_LL = 
+%S_LL = not given!
 
 %Theoretical standard deviation
 %sigma_0 =      %a priori
 
 %Cofactor matrix of the observations
-%Q_LL = 
+Q_LL = eye(no_n);
 
 %Weight matrix
-%P = 
+P = inv(Q_LL);
 
 %--------------------------------------------------------------------------
 %  Adjustment
 %--------------------------------------------------------------------------
 %break-off conditions
-%epsilon = 
-%delta = 
-%max_x_hat = 
+epsilon = 10^-12;
+delta = 10^-12;
+max_x_hat = Inf;
+Check2 = Inf;
 
 %Number of iterations
 iteration = 0;
 
-%while            
+while max_x_hat>epsilon || Check2>delta
     
      %Observations as functions of the approximations for the unknowns
-%     L_0 = 
+    L_0 = [x+y-2*y^2; x^2+y^2; 3*x^2-y^2];
 
      
      %Vector of reduced observations
-%     l = 
+     l = L - L_0;
     
      %Design matrix with the elements from the Jacobian matrix J
-%     A = 
+     A = [1 1-4*y; 2*x 2*y; 6*x -2*y];
     
      %Normal matrix
-%     N = 
+     N = A'*P*A;
      
      %Vector of right hand side of normal equations
-%     n = 
+     n = A'*P*l;
     
      %Inversion of normal matrix / Cofactor matrix of the unknowns
-%     Q_xx = 
+     Q_xx = inv(N);
     
      %Solution of the normal equations
-%     x_hat = 
+     x_hat = Q_xx*n;
        
      %Update
-%     X_hat =
-%     X_0 = X_hat;
+     X_0 =  X_0+x_hat;
     
-%     x = 
-%     y = 
+     x = X_0(1);
+     y = X_0(2);
+
     
      %Check 1
-%     max_x_hat 
+    max_x_hat = max(abs(x_hat));
      
      %Vector of residuals
-%     v = 
+     v = A*x_hat-l;
  
      %Vector of adjusted observations
-%     L_hat = 
+     L_hat = L+v;
     
      %Objective function
-%     vTPv = 
+     vTPv = v'*P*v;
     
-
 
      %Check 2
-%     Check2 = 
-    
+     Check2 = max(abs(L_hat-[x+y-2*y^2; x^2+y^2; 3*x^2-y^2]));
+    %this check not so necessery, its suffieciet to just check dX.
+
      %Update number of iterations
      iteration = iteration+1;
   
-%end
+end
 
-
+%rest copy from previous exercise:
 
 %Empirical reference standard deviation
 %s_0 = 
